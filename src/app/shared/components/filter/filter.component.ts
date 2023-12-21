dc27f2f711ee5134b7fc72b348e754ac19bd0fda
Ramon Ribeiro <ramonrib@gmail.com>
2023-12-21 14:07:56 -0300

    realocation constant types in file

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { TYPES } from '../../models/constants/pokemons-types';
 
 @Component({
   selector: 'app-filter',
@@ -8,80 +9,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class FilterComponent {
   @Output() filter = new EventEmitter();
 
-  types = [
-    {
-      name: 'bug',
-      src: '../../../../assets/types-icons/bug.svg',
-    },
-    {
-      name: 'dark',
-      src: '../../../../assets/types-icons/dark.svg',
-    },
-    {
-      name: 'dragon',
-      src: '../../../../assets/types-icons/dragon.svg',
-    },
-    {
-      name: 'electric',
-      src: '../../../../assets/types-icons/electric.svg',
-    },
-    {
-      name: 'fairy',
-      src: '../../../../assets/types-icons/fairy.svg',
-    },
-    {
-      name: 'fighting',
-      src: '../../../../assets/types-icons/fighting.svg',
-    },
-    {
-      name: 'fire',
-      src: '../../../../assets/types-icons/fire.svg',
-    },
-    {
-      name: 'flying',
-      src: '../../../../assets/types-icons/flying.svg',
-    },
-    {
-      name: 'ghost',
-      src: '../../../../assets/types-icons/ghost.svg',
-    },
-    {
-      name: 'grass',
-      src: '../../../../assets/types-icons/grass.svg',
-    },
-    {
-      name: 'ground',
-      src: '../../../../assets/types-icons/ground.svg',
-    },
-    {
-      name: 'ice',
-      src: '../../../../assets/types-icons/ice.svg',
-    },
-    {
-      name: 'normal',
-      src: '../../../../assets/types-icons/normal.svg',
-    },
-    {
-      name: 'poison',
-      src: '../../../../assets/types-icons/poison.svg',
-    },
-    {
-      name: 'psychic',
-      src: '../../../../assets/types-icons/psychic.svg',
-    },
-    {
-      name: 'rock',
-      src: '../../../../assets/types-icons/rock.svg',
-    },
-    {
-      name: 'steel',
-      src: '../../../../assets/types-icons/steel.svg',
-    },
-    {
-      name: 'water',
-      src: '../../../../assets/types-icons/water.svg',
-    },
-  ];
+  types = TYPES;
 
   filtering(name) {
     this.filter.emit(name);
---
+++ src/app/shared/models/constants/pokemons-types.ts
@@ -0,0 +1,74 @@
+export const TYPES = [
+  {
+    name: 'bug',
+    src: '../../../../assets/icons/bug.svg',
+  },
+  {
+    name: 'dark',
+    src: '../../../../assets/icons/dark.svg',
+  },
+  {
+    name: 'dragon',
+    src: '../../../../assets/icons/dragon.svg',
+  },
+  {
+    name: 'electric',
+    src: '../../../../assets/icons/electric.svg',
+  },
+  {
+    name: 'fairy',
+    src: '../../../../assets/icons/fairy.svg',
+  },
+  {
+    name: 'fighting',
+    src: '../../../../assets/icons/fighting.svg',
+  },
+  {
+    name: 'fire',
+    src: '../../../../assets/icons/fire.svg',
+  },
+  {
+    name: 'flying',
+    src: '../../../../assets/icons/flying.svg',
+  },
+  {
+    name: 'ghost',
+    src: '../../../../assets/icons/ghost.svg',
+  },
+  {
+    name: 'grass',
+    src: '../../../../assets/icons/grass.svg',
+  },
+  {
+    name: 'ground',
+    src: '../../../../assets/icons/ground.svg',
+  },
+  {
+    name: 'ice',
+    src: '../../../../assets/icons/ice.svg',
+  },
+  {
+    name: 'normal',
+    src: '../../../../assets/icons/normal.svg',
+  },
+  {
+    name: 'poison',
+    src: '../../../../assets/icons/poison.svg',
+  },
+  {
+    name: 'psychic',
+    src: '../../../../assets/icons/psychic.svg',
+  },
+  {
+    name: 'rock',
+    src: '../../../../assets/icons/rock.svg',
+  },
+  {
+    name: 'steel',
+    src: '../../../../assets/icons/steel.svg',
+  },
+  {
+    name: 'water',
+    src: '../../../../assets/icons/water.svg',
+  },
+];
