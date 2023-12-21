63cf379a54e53c1d9cdc9a090fe3e43b0a66c0f1
Ramon Ribeiro <ramonrib@gmail.com>
2023-12-21 14:22:01 -0300

    refact removing pipe uppercaseFirst

--- src/app/feature/pokemon.module.ts
+++ src/app/feature/pokemon.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonRoutingModule } from './pokemon-routing.module';
 import { CardComponent } from '../shared/components/card/card.component';
-import { UppercaseFirstPipe } from 'src/app/shared/pipes/uppercaseFirst.pipe';
 import { PaginationComponent } from 'src/app/shared/components/pagination/pagination.component';
 import { FilterComponent } from 'src/app/shared/components/filter/filter.component';
 import { SortByOrderPipe } from '../shared/pipes/sortByOrder.pipe';
@@ -13,7 +12,6 @@ import { SortByOrderPipe } from '../shared/pipes/sortByOrder.pipe';
   declarations: [
     PokemonListComponent,
     CardComponent,
-    UppercaseFirstPipe,
     SortByOrderPipe,
     PaginationComponent,
     FilterComponent,
